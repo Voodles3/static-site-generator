e54ebcc3bff31bb1c3954eb964fd0948da230589
Vaughn Christman <vaughnjchristman+dev@gmail.com>
2026-04-15 10:55:15 -0700

    feat: add main.copy_dir to copy directory contents to a destination folder

--- src/main.py
+++ src/main.py
@@ -1,9 +1,38 @@
-from nodes.textnode import TextNode, TextType
+import os
+import shutil
+from pathlib import Path
 
 
 def main():
-    new = TextNode("some text here boi", TextType.TEXT)
-    print(new)
+    copy_dir(Path("static/"), Path("public/"))
+
+
+def copy_dir(src: Path, dest: Path) -> None:
+    if not os.path.exists(src):
+        raise FileNotFoundError("Source directory not found")
+
+    if not os.path.exists(dest):
+        os.mkdir(dest)
+    else:
+        dir_contents = os.listdir(dest)
+        for name in dir_contents:
+            path = os.path.join(dest, name)
+            if os.path.isfile(path):
+                os.remove(path)
+            else:
+                shutil.rmtree(path)
+
+    contents = os.listdir(src)
+    for name in contents:
+        path = os.path.join(src, name)
+
+        if os.path.isfile(path):
+            print(f"COPYING FILE {path} TO {dest}")
+            shutil.copy(path, dest)
+        else:
+            dir_name = os.path.join(dest, name)
+            print(f"COPYING DIR {path} TO {dir_name}")
+            copy_dir(Path(path), Path(dir_name))
 
 
 if __name__ == "__main__":
